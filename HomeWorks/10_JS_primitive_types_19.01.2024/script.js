e2322f74953da97339c8baf9210c9e2e027cfd86
Martins Groza <mar4inja19@gmail.com>
2024-01-22 11:20:09 +0100

    update

--- HomeWorks/10_JS_primitive_types_19.01.2024/script.js
+++ HomeWorks/10_JS_primitive_types_19.01.2024/script.js
@@ -1,23 +1,62 @@
-
 res = sumDigits(1234); // 1 + 2 + 3 + 4 = 10
+console.log(res);
 
 function sumDigits(x) {
-  res = 1 + 2 + 3 + 4;
-  console.log(res);
+  let sum = 0;
+  while (x) {
+    // x != 0
+    sum += x % 10; // sum = sum + (x % 10)
+    x = (x - (x % 10)) / 10;
+  }
+  return sum;
 }
 
-res = luckyNumber(123871); // 1 + 2 + 3 == 8 + 7 + 1
+res = luckyNumber(123321); // 1 + 2 + 3 == 8 + 7 + 1
+console.log(res);
 
 function luckyNumber(x) {
-  x = 1 + 2 + 3 === 8 + 7 + 1;
-  console.log(x);
-}
+  let numberLength = (x + "").length;
 
+  let halfLength = numberLength / 2 - ((numberLength / 2) % 1);
+  console.log(halfLength);
+  console.log(numberLength);
+  const secondHalf = x % 10 ** halfLength;
+  const firstHalf = (x - secondHalf) / 10 ** halfLength;
+  return sumDigits(firstHalf) == sumDigits(secondHalf);
+
+  // if ((x + "").length === 6) {
+  //   const secondHalf = x % 1000;
+  //   const firstHalf = (x - secondHalf) / 1000;
+  //   return sumDigits(firstHalf) == sumDigits(secondHalf);
+  // } else {
+  //   return "Wrong number";
+  // }
+}
 function banana() {
   let a = "a";
   let b = "b";
 
-res = b + a +  + a + a ; 
-console.log(res);
+  res = (b + a + +a + a).toLocaleLowerCase();
+  console.log(res);
 }
 banana();
+
+function fourOrSeven(x) {
+  // if (x === 4) {
+  //   return 7
+  // }else{
+  //   return 4;
+  // }
+  // return x === 4 ? 7 : 4;
+  // }
+
+  // switch (x) {
+  //   case 4:
+  //     return 4;
+  //   default:
+  //     return 7;
+  // }
+  // return 11 - x;
+
+  // return 28 / x;
+}
