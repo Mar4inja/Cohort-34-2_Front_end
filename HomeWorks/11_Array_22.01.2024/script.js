0a208890f62548e9456149483e26e68740d9e833
Martins Groza <mar4inja19@gmail.com>
2024-01-23 10:02:40 +0100

    update

--- HomeWorks/11_Array_22.01.2024/script.js
+++ HomeWorks/11_Array_22.01.2024/script.js
@@ -9,3 +9,31 @@ if (names.length === ages.length) {
   }
   console.log(result);
 }
+
+console.log("*****Second resolve*****");
+
+const names2 = ["Семен", "Иван", "Петр", "Татьяна"];
+const ages2 = [18, 27, 74, 34];
+const result = [];
+
+for (let i = 0; i < names2.length; i++) {
+  result.push(names2[i] + ":" + ages2[i] + " years old");
+  
+  // result = names2[i] + ":" + ages2[i] + " years old";
+}
+
+console.log(result);
+
+
+console.log("*****Third resolve*****");
+
+const names3 = ["Семен", "Иван", "Петр", "Татьяна"];
+const ages3 = [18, 27, 74, 34];
+const result3 = [];
+
+for (const name of names3 ) {
+  console.log(name);
+ 
+  }
+
+console.log(result);
