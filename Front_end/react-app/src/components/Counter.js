05901cdbdd1cadf6673cd871d75f7914535c51f6
Martins Groza <mar4inja19@gmail.com>
2024-02-05 21:55:26 +0100

    update

--- Front_end/react-app/src/components/Counter.js
+++ Front_end/react-app/src/components/Counter.js
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = React.useState(0);
+  const [inputValue, setInputValue] = useState("");
 
   //setCount(1)  меняется значение локального состояния (count) с помощью специального метода setCount
 
-//   let x = 0;
-//   console.log(x--);
+  //   let x = 0;
+  //   console.log(x--);
 
-const increment = () => {
-    setCount(count +1);
-}
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const handleSubmit = () => {
+    const parsedValue = parseInt(inputValue);
+    if (!isNaN(parsedValue)) {
+      setCount(parsedValue);
+      setInputValue("");
+    }
+  };
 
   return (
     <div>
@@ -18,14 +27,17 @@ const increment = () => {
       <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
       <button onClick={() => setCount(increment)}>Increment</button>
-     
-   <div>
-   <input type="text" placeholder="Set new counter value...">
-   </input>
-   <button onClick={() => setCount()}>Submit</button>
-   
+
+      <div>
+        <input
+          type="text"
+          placeholder="Set new counter value..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={() => setCount(handleSubmit)}>Submit</button>
+      </div>
     </div>
-   </div>
   );
 }
 
